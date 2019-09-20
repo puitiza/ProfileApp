811357db535c569bccd971e12c1e7ba453866c1a
Junior Meza <junior.meza@forceclose.pe>
2019-09-20 11:09:50 -0500

    Initial Commit

---
+++ ProfileApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProfileApp
+//
+//  Created by Junior Meza on 9/20/19.
+//  Copyright © 2019 forceclose. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
